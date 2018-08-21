51e5a75a77c0f40d808057227d8580f5711926b0
Ruiferr <rui.diogo66@gmail.com>
2018-08-21 21:59:25 +0100

    Basics: problem 4.

--- js/basics.js
+++ js/basics.js
@@ -36,7 +36,7 @@ function printContent(){
 
 
 
-*/
+
 
 // 3. Write a JavaScript program to get the current date.
 // Output : 
@@ -56,9 +56,25 @@ function dateFormat(){
 
 }
 
+*/
+
+//4. Write a JavaScript program to find the area of a triangle 
+//   where lengths of the three of its sides are 5, 6, 7.  
 
 
+triangleArea()
 
+function triangleArea(){
+
+	var side1 = parseInt(prompt("Insert first triangle side lenght:", "5"));
+	var side2 = parseInt(prompt("Insert second triangle side lenght:", "6"));
+	var side3 = parseInt(prompt("Insert third triangle side lenght:", "7"));
+
+	var p = (side1 + side2 + side3)/2;
+	var area = Math.sqrt(p*(p-side1)*(p-side2)*(p-side3));
+
+	document.getElementById("content").innerHTML = "The area of the triangle is: "+ area;
+}
 
 
 
