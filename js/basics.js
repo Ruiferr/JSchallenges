4fe50f29e39d22df4cf8e39ef26bd935d864b53c
Ruiferr <rui.diogo66@gmail.com>
2018-08-21 21:34:17 +0100

    Basics: problem 1.

--- js/basics.js
+++ js/basics.js
@@ -1,3 +1,29 @@
+// 1. Write a JavaScript program to display the current day and time in the following format.
+// Output : 
+// Today is : Tuesday. 
+// Current time is : 10 PM : 30 : 38
+
+
+getTime();
+
+function getTime(){
+
+	var d = new Date();
+	var day = d.getDay(); 
+	var hour = d.getHours() + (d.getHours() >= 12 ? " PM" : " AM");
+	var minutes = d.getMinutes();
+	var seconds = d.getSeconds();
+
+	//get day of the week
+	var weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+	//write to html
+	document.getElementById("content").innerHTML = "Today is : "+ weekDay[day]+ 
+												   "<br> Current time is : " + hour +" : "+ minutes +": "+ seconds+"";
+
+}
+
+
 
 
 
